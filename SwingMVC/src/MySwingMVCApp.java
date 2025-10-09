cd3d15dd0fff3605df63b97ad67f29f0ca44357c
isamsami <96512947+isamsami@users.noreply.github.com>
2025-10-08 23:31:45 -0400

    Update MySwingMVCApp.java

--- SwingMVC/src/MySwingMVCApp.java
+++ SwingMVC/src/MySwingMVCApp.java
@@ -4,12 +4,12 @@ public class MySwingMVCApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  // Assemble all the pieces of the MVC
-		  Model m = new Model("Sylvain", "Saurel");
-		  View v = new View("MVC with SSaurel");
-		  Controller c = new Controller(m, v);
+		 Model m = new Model("Sylvain", "Saurel", "productData.txt");
+		  View v = new View("MVC with Cash Register");
+		  Controller c = new Controller(m, v, s);
 		  
 		  // create scanner
-		  Scanner scanner = new Scanner();
+		  Scanner s = new Scanner();
 		  
 		  c.initController();
 	}
